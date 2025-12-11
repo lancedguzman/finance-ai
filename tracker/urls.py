d7c2713819a74cea0b7d1c5964eec9d078698596
Lance B. De Guzman <137474979+lancedguzman@users.noreply.github.com>
2025-12-11 10:44:12 +0800

    bug(form): edit form not working
    - adjust the edit button to work
    - removed "telegram" and "scanner" for next version

--- tracker/urls.py
+++ tracker/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('tracker/', views.tracker, name='tracker'),
     path('api/transactions/', views.transaction_list, name='transaction_list'),
     path('add-transaction/', views.add_transaction, name='add_transaction'),
+    path('edit-transaction/<int:id>/', views.edit_transaction, name='edit_transaction'),
     path('delete-transaction/<int:id>/', views.delete_transaction, name='delete_transaction'),
     path('add-category/', views.add_category, name='add_category'),
 ]
